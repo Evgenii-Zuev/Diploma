e6be9b5c9e6dc65911bc2d468b483ecf36cb69b1
Evgenii-Zuev <98023720+Evgenii-Zuev@users.noreply.github.com>
2022-02-01 09:06:13 +0400

    Add files via upload

---
+++ csv_to_sql.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+import csv
+mydb = mysql.connector.connect(host='localhost',
+    user='root',
+    passwd='GE*5if2n',
+    db='test')
+cursor = mydb.cursor()
+with open('out.txt') as csv_file:   
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  query = ("INSERT INTO the_beatles "
+      "(kind,collectionName,trackName,collectionPrice,trackPrice,primaryGenreName,trackCount,trackNumber,releaseDate) "
+      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+  for data in csv_reader:
+#      print(data)
+      cursor.execute(query, data)
+#####close the connection to the database.
+mydb.commit()
+cursor.close()
+print("Done")
