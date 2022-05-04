f4ba8e785191d865ed72eff492be170bd7cfb480
Evgenii Zuev <zuev1966@mail.ru>
2022-05-04 20:22:09 +0400

    modified:   the server part is pre-installed/app.py

--- the server part is pre-installed/app.py
+++ the server part is pre-installed/app.py
@@ -39,9 +39,9 @@ import mysql.connector
 from mysql.connector import MySQLConnection, Error
 import csv
 mydb = mysql.connector.connect(host=os.getenv('mysql_host'),
-    user=os.getenv('mysql_user'),
-    passwd=os.getenv('mysql_password'),
-    db=os.getenv('mysql_db'))
+                               user=os.getenv('mysql_user'),
+                               passwd=os.getenv('mysql_password'),
+                               db=os.getenv('mysql_db'))
 
 cursor = mydb.cursor()
 query = ("DELETE FROM the_beatles")
